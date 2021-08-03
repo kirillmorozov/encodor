b7f3efcb1de44c008b8b8cabdb169efe2e5e3864
Kirill Morozov <kirill@robotix.pro>
2021-08-03 22:17:42 +0300

    Add test case for multiline text

--- beghilosz_test.go
+++ beghilosz_test.go
@@ -18,6 +18,7 @@ func TestBeghilosz(t *testing.T) {
 		{name: "Hashtags untouched, but capitalized", args: args{"#hashtag"}, want: "#HASHTAG"},
 		{name: "Usernames untouched, but capitalized", args: args{"@username"}, want: "@USERNAME"},
 		{name: "Word order in a sentence with hashtag", args: args{"Sentence with a #hashtag"}, want: "#HASHTAG A 4T1W 3CN3TN35"},
+		{name: "Multiline text", args: args{"Line 1\nLine 2"}, want: "2 3N17\n1 3N17"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
