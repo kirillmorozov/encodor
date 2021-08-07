4b816e1be1348eaf73a86c12220272bf81c603df
Kirill Morozov <kirill@robotix.pro>
2021-08-08 00:48:52 +0300

    Add docstrings

--- beghilosz.go
+++ beghilosz.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// beghiloszReplacer replace all runes according to beghilosz encoding.
 var beghiloszReplacer = strings.NewReplacer(
 	"B", "8",
 	"E", "3",
@@ -16,6 +17,9 @@ var beghiloszReplacer = strings.NewReplacer(
 	"Z", "2",
 )
 
+// Beghilosz encode text to calculator spelling.
+// Hashtags(words beginning with '#') and mentions(words beginning with '@') are
+// left as is.
 func Beghilosz(text string) string {
 	text = strings.ToUpper(text)
 	lines := strings.Split(text, "\n")
@@ -28,19 +32,21 @@ func Beghilosz(text string) string {
 			}
 			words[wordIndex] = word
 		}
-		words = reverseSlice(words)
+		words = reverseStringSlice(words)
 		lines[lineIndex] = strings.Join(words, " ")
 	}
-	lines = reverseSlice(lines)
+	lines = reverseStringSlice(lines)
 	return strings.Join(lines, "\n")
 }
 
+// isSpecialWord return true if word is a hashtag or a username.
 func isSpecialWord(word string) bool {
 	isHashtag := strings.HasPrefix(word, "#")
 	isUsername := strings.HasPrefix(word, "@")
 	return isHashtag || isUsername
 }
 
+// reverseString reverse rune order in str.
 func reverseString(str string) string {
 	rns := []rune(str)
 	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
@@ -49,7 +55,8 @@ func reverseString(str string) string {
 	return string(rns)
 }
 
-func reverseSlice(slice []string) []string {
+// reverseStringSlice reverse strings order in slice.
+func reverseStringSlice(slice []string) []string {
 	for i := 0; i < len(slice)/2; i++ {
 		slice[i], slice[len(slice)-1-i] = slice[len(slice)-1-i], slice[i]
 	}
