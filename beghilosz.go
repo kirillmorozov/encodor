fefba68e5cb8bf5cd2fe4fe344345ff8e28273e9
Kirill Morozov <kirill@robotix.pro>
2021-08-08 00:49:59 +0300

    Refactor `Beghilosz` (#13)
    - Fix encoding of multiline texts
    - Add benchmark for Beghilosz
    - Improve performance
    - Rename variables and functions
    - Add docstrings

--- beghilosz.go
+++ beghilosz.go
@@ -4,53 +4,61 @@ import (
 	"strings"
 )
 
-func beghilosz_mapping(letter rune) rune {
-	var char_map = map[rune]rune{
-		'B': '8',
-		'E': '3',
-		'G': '6',
-		'H': '4',
-		'I': '1',
-		'L': '7',
-		'O': '0',
-		'S': '5',
-		'Z': '2',
-	}
-	new_letter, exists := char_map[letter]
-	if !exists {
-		return letter
-	} else {
-		return new_letter
-	}
-}
+// beghiloszReplacer replace all runes according to beghilosz encoding.
+var beghiloszReplacer = strings.NewReplacer(
+	"B", "8",
+	"E", "3",
+	"G", "6",
+	"H", "4",
+	"I", "1",
+	"L", "7",
+	"O", "0",
+	"S", "5",
+	"Z", "2",
+)
 
-func Beghilosz(input string) string {
-	words := strings.Fields(input)
-	encoded_words := make([]string, len(words))
-	for i, word := range words {
-		word = strings.ToUpper(word)
-		if !(strings.HasPrefix(word, "#") || strings.HasPrefix(word, "@")) {
-			word = strings.Map(beghilosz_mapping, word)
-			word = reverse(word)
+// Beghilosz encode text to calculator spelling.
+// Hashtags(words beginning with '#') and mentions(words beginning with '@') are
+// left as is.
+func Beghilosz(text string) string {
+	text = strings.ToUpper(text)
+	lines := strings.Split(text, "\n")
+	for lineIndex, line := range lines {
+		words := strings.Fields(line)
+		for wordIndex, word := range words {
+			if !isSpecialWord(word) {
+				word = beghiloszReplacer.Replace(word)
+				word = reverseString(word)
+			}
+			words[wordIndex] = word
 		}
-		encoded_words[i] = word
+		words = reverseStringSlice(words)
+		lines[lineIndex] = strings.Join(words, " ")
 	}
-	encoded_words = reverseSlice(encoded_words)
-	output := strings.Join(encoded_words, " ")
-	return output
+	lines = reverseStringSlice(lines)
+	return strings.Join(lines, "\n")
 }
 
-func reverse(input string) string {
-	var result string
-	for _, letter := range input {
-		result = string(letter) + result
-	}
-	return result
+// isSpecialWord return true if word is a hashtag or a username.
+func isSpecialWord(word string) bool {
+	isHashtag := strings.HasPrefix(word, "#")
+	isUsername := strings.HasPrefix(word, "@")
+	return isHashtag || isUsername
 }
 
-func reverseSlice(input []string) []string {
-	for i := 0; i < len(input)/2; i++ {
-		input[i], input[len(input)-1-i] = input[len(input)-1-i], input[i]
+// reverseString reverse rune order in str.
+func reverseString(str string) string {
+	rns := []rune(str)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
 	}
-	return input
+	return string(rns)
+}
+
+// reverseStringSlice reverse strings order in slice.
+func reverseStringSlice(slice []string) []string {
+	for i := 0; i < len(slice)/2; i++ {
+		slice[i], slice[len(slice)-1-i] = slice[len(slice)-1-i], slice[i]
+	}
+	return slice
 }
--- beghilosz_test.go
+++ beghilosz_test.go
@@ -18,11 +18,43 @@ func TestBeghilosz(t *testing.T) {
 		{name: "Hashtags untouched, but capitalized", args: args{"#hashtag"}, want: "#HASHTAG"},
 		{name: "Usernames untouched, but capitalized", args: args{"@username"}, want: "@USERNAME"},
 		{name: "Word order in a sentence with hashtag", args: args{"Sentence with a #hashtag"}, want: "#HASHTAG A 4T1W 3CN3TN35"},
+		{name: "Multiline text", args: args{"Line 1\nLine 2"}, want: "2 3N17\n1 3N17"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Beghilosz(tt.args.input); got != tt.want {
 				t.Errorf("Beghilosz() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkBeghilosz(b *testing.B) {
+	const test_string string = "word1.1 word1.2 @username1 #hashtag1\nword2.1 word2.2 @username2 #hashtag2"
+	b.Run("Multiline", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			Beghilosz(test_string)
+		}
+	})
+}
+
+func Test_isSpecialWord(t *testing.T) {
+	type args struct {
+		word string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "Hashtag", args: args{"#nofilter"}, want: true},
+		{name: "Username", args: args{"@username"}, want: true},
+		{name: "Regular word", args: args{"word"}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSpecialWord(tt.args.word); got != tt.want {
+				t.Errorf("isSpecialWord() = %v, want %v", got, tt.want)
 			}
 		})
 	}
