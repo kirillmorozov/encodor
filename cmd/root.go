08bc89687033f16dd3660bf196784e4cb420f4e8
Kirill Morozov <kirill.morozov@native-robotics.com>
2022-05-26 11:49:10 +0300

    refactor: Remove global variables (#25)

--- cmd/root.go
+++ cmd/root.go
@@ -1,29 +1,26 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Short: "Encodes your input",
+func NewRoot() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Short: "Encodes your input",
+	}
+	beghiloszCmd := NewBeghiloszCmd()
+	rootCmd.AddCommand(beghiloszCmd)
+	zalgoCmd := NewZalgoCmd()
+	rootCmd.AddCommand(zalgoCmd)
+	return rootCmd
 }
 
 func Execute() {
+	rootCmd := NewRoot()
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		rootCmd.PrintErr(err)
 		os.Exit(1)
 	}
-}
-
-func ExecuteCustomIO(args []string, output io.Writer) error {
-	rootCmd.SetArgs(args)
-	rootCmd.SetOutput(output)
-	if err := rootCmd.Execute(); err != nil {
-		return err
-	}
-	return nil
 }
--- cmd/zalgo.go
+++ cmd/zalgo.go
@@ -7,23 +7,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(zalgoCmd)
-}
+func NewZalgoCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "zalgo word...",
+		Aliases: []string{"z"},
+		Short:   "Turn input words into criptic zalgo text",
+		Long: `Turn input words into criptic zalgo text.
 
-var zalgoCmd = &cobra.Command{
-	Use:     "zalgo word...",
-	Aliases: []string{"z"},
-	Short:   "Turn input words into criptic zalgo text",
-	Long: `Turn input words into criptic zalgo text.
-	
 Zalgo text is digital text that has been modified with combining characters,
 Unicode symbols used to add diacritics above or below letters, to appear
 frightening or glitchy.`,
-	Example: "encodor zalgo Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
-	Run: func(cmd *cobra.Command, args []string) {
-		text := strings.Join(args, " ")
-		encoded := zalgo.Encode(text)
-		cmd.Println(encoded)
-	},
+		Example: "encodor zalgo Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
+		Run: func(cmd *cobra.Command, args []string) {
+			text := strings.Join(args, " ")
+			encoded := zalgo.Encode(text)
+			cmd.Println(encoded)
+		},
+	}
 }
