1ad9fdefe9ad09d1820432f7bee5a2b8a71ce3d6
Kirill Morozov <kirill.morozov@native-robotics.com>
2022-05-27 22:42:31 +0300

    feat(zalgo): Add `diacritics` flag
    Add diacritics flag to cmd to control how many diacritics are added to
    each letter. (default 1)

--- cmd/zalgo.go
+++ cmd/zalgo.go
@@ -7,21 +7,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func NewZalgoCmd() *cobra.Command {
-	return &cobra.Command{
-		Use:     "zalgo word...",
-		Aliases: []string{"z"},
-		Short:   "Turn input words into criptic zalgo text",
-		Long: `Turn input words into criptic zalgo text.
+const (
+	zalgoUse   = "zalgo word..."
+	zalgoShort = "Turn input words into criptic zalgo text"
+	zalgoLong  = `Turn input words into criptic zalgo text.
 
-Zalgo text is digital text that has been modified with combining characters,
-Unicode symbols used to add diacritics above or below letters, to appear
-frightening or glitchy.`,
-		Example: "encodor zalgo Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
+Zalgo text is digital text that has been modified with combining characters, Unicode symbols used to add diacritics above or below letters, to appear frightening or glitchy.`
+	zalgoExample    = "encodor zalgo -d 3 Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn"
+	diacriticsUsage = `How many diacritics are added to each letter. Should be 1 <= diacritics <= 3`
+)
+
+func NewZalgoCmd() *cobra.Command {
+	zalgoCmd := &cobra.Command{
+		Use:     zalgoUse,
+		Aliases: []string{"z"},
+		Short:   zalgoShort,
+		Long:    zalgoLong,
+		Example: zalgoExample,
 		Run: func(cmd *cobra.Command, args []string) {
 			text := strings.Join(args, " ")
-			encoded := zalgo.Encode(text)
+			strength, flgErr := cmd.Flags().GetInt8("diacritics")
+			if flgErr != nil {
+				cmd.PrintErr(flgErr)
+			}
+			encoded, encodingErr := zalgo.Encode(text, strength)
+			if encodingErr != nil {
+				cmd.PrintErr(encodingErr)
+			}
 			cmd.Println(encoded)
 		},
 	}
+	zalgoCmd.Flags().Int8P("diacritics", "d", 1, diacriticsUsage)
+	return zalgoCmd
 }
