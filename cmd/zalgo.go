25a18a853844b9860f79f1d69914408cf7aa4c7f
Kirill Morozov <kirill@robotix.pro>
2022-05-28 11:16:05 +0300

    refactor(zalgo): Remove string contants from cmd

--- cmd/zalgo.go
+++ cmd/zalgo.go
@@ -8,29 +8,20 @@ import (
 )
 
 const (
-	zalgoUse   = "zalgo word..."
-	zalgoShort = "Turn input words into criptic zalgo text"
-	zalgoLong  = `Turn input words into criptic zalgo text.
-
-Zalgo text is digital text that has been modified with combining characters,
-Unicode symbols used to add diacritics above or below letters, to appear
-frightening or glitchy.`
-	zalgoExample    = "encodor zalgo -d 3 Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn"
-	diacriticsUsage = `How many diacritics are added to each letter.
-Should be 1 <= diacritics <= 5`
-)
-
-const (
 	defaultDiacritics = 3
 )
 
 func NewZalgoCmd() *cobra.Command {
 	zalgoCmd := &cobra.Command{
-		Use:     zalgoUse,
+		Use:     "zalgo word...",
 		Aliases: []string{"z"},
-		Short:   zalgoShort,
-		Long:    zalgoLong,
-		Example: zalgoExample,
+		Short:   "Turn input words into criptic zalgo text",
+		Long: `Turn input words into criptic zalgo text.
+
+Zalgo text is digital text that has been modified with combining characters,
+Unicode symbols used to add diacritics above or below letters, to appear
+frightening or glitchy.`,
+		Example: "encodor zalgo -d 3 Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
 		Run: func(cmd *cobra.Command, args []string) {
 			text := strings.Join(args, " ")
 			strength, flgErr := cmd.Flags().GetInt8("diacritics")
@@ -44,6 +35,11 @@ func NewZalgoCmd() *cobra.Command {
 			cmd.Println(encoded)
 		},
 	}
-	zalgoCmd.Flags().Int8P("diacritics", "d", defaultDiacritics, diacriticsUsage)
+	zalgoCmd.Flags().Int8P(
+		"diacritics",
+		"d",
+		defaultDiacritics,
+		`How many diacritics are added to each letter.
+Should be 1 <= diacritics <= 5`)
 	return zalgoCmd
 }
